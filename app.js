d573247a5bf411e61d00bbc20bd643177cd649a2
Saharat Thongin <6306021620191@fitm.kmutnb.ac.th>
2023-08-10 00:13:58 +0700

    Implement PWA

--- app.js
+++ app.js
@@ -90,5 +90,10 @@ gradeFormat = (grade) =>{
 
 }
 
-addTableRow()
-console.log("finish")
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./serviceWorker.js").then(registration => {
+      console.log("Service Worker registered with scope:", registration.scope);
+    }).catch(error => {
+      console.log("Service Worker registration failed:", error);
+    });
+  }
